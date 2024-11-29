ff4f62627d87277c905111bdfa18087afee93a08
Matthieu MESNAGER <38081186+MatthieuMesnager@users.noreply.github.com>
2024-11-29 23:32:31 +0100

    Revert "feat(credits-controller): Adds the route and the credits page controller."
    This reverts commit 7aae4f296a5487bb13b8242bec8f7e1dd35b5d46.

--- src/routes/web.php
+++ src/routes/web.php
@@ -3,10 +3,8 @@
 declare(strict_types=1);
 
 use App\Http\Controllers\HomeController;
-use App\Http\Controllers\CreditsController;
 use App\Http\Controllers\PlaceholderImageController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', HomeController::class)->name('page.home');
-Route::get('/credits', CreditsController::class)->name('page.credits');
 Route::get('/{width}/{height?}', PlaceholderImageController::class);
