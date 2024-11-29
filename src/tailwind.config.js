f1430bdeca8ff5b2b97ed9a91614d7e266777212
Matthieu MESNAGER <38081186+MatthieuMesnager@users.noreply.github.com>
2024-11-29 23:32:31 +0100

    Revert "config(tailwindcss): Adds TailwindCSS config file."
    This reverts commit 069a21fe861ea8f22df58a5b89ff880113fc7af0.

--- src/tailwind.config.js
+++ src/tailwind.config.js
@@ -1,30 +1,18 @@
-const defaultTheme = require("tailwindcss/defaultTheme");
+import defaultTheme from 'tailwindcss/defaultTheme';
 
-/** @type {import("tailwindcss").Config} */
+/** @type {import('tailwindcss').Config} */
 export default {
-    darkMode: "selector",
     content: [
-        "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
-        "./storage/framework/views/*.php",
-        "./resources/**/*.blade.php",
-        "./resources/**/*.js",
+        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+        './storage/framework/views/*.php',
+        './resources/**/*.blade.php',
+        './resources/**/*.js',
+        './resources/**/*.vue',
     ],
     theme: {
-        boxShadow: {
-            md: "0 4px 5px 0 rgba(0,0,0,0.14),0 1px 10px 0 rgba(0,0,0,0.12),0 2px 4px -1px rgba(0,0,0,0.3)",
-            lg: "0 8px 17px 2px rgba(0,0,0,0.14),0 3px 14px 2px rgba(0,0,0,0.12),0 5px 5px -3px rgba(0,0,0,0.2)",
-        },
         extend: {
-            colors: {
-                zinc: {
-                    900: "#1a1c1d",
-                    950: "#161718",
-                },
-            },
-            container: {
-                screens: {
-                    "2xl": defaultTheme.screens.xl,
-                },
+            fontFamily: {
+                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
             },
         },
     },
