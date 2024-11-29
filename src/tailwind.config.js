043dbc6923496abf0cc382e2a3ab29dcfb4cc71a
Matthieu MESNAGER <38081186+MatthieuMesnager@users.noreply.github.com>
2024-11-29 23:32:31 +0100

    Revert "chore(tailwind-config): Moves the `boxShadow` config to the extend key."
    This reverts commit 52df5997eda4e5f60fcaf4f5fe45f335d5e6afd9.

--- src/tailwind.config.js
+++ src/tailwind.config.js
@@ -10,11 +10,11 @@ export default {
         "./resources/**/*.js",
     ],
     theme: {
+        boxShadow: {
+            md: "0 4px 5px 0 rgba(0,0,0,0.14),0 1px 10px 0 rgba(0,0,0,0.12),0 2px 4px -1px rgba(0,0,0,0.3)",
+            lg: "0 8px 17px 2px rgba(0,0,0,0.14),0 3px 14px 2px rgba(0,0,0,0.12),0 5px 5px -3px rgba(0,0,0,0.2)",
+        },
         extend: {
-            boxShadow: {
-                md: "0 4px 5px 0 rgba(0,0,0,0.14),0 1px 10px 0 rgba(0,0,0,0.12),0 2px 4px -1px rgba(0,0,0,0.3)",
-                lg: "0 8px 17px 2px rgba(0,0,0,0.14),0 3px 14px 2px rgba(0,0,0,0.12),0 5px 5px -3px rgba(0,0,0,0.2)",
-            },
             colors: {
                 zinc: {
                     900: "#1a1c1d",
