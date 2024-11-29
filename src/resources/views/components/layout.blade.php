99e5cea98acb699ac1f93d634e07834ebe7ea6b5
Matthieu MESNAGER <38081186+MatthieuMesnager@users.noreply.github.com>
2024-11-29 23:32:31 +0100

    Revert "chore(layout-view): Removes unused css file include."
    This reverts commit 6ad68c3ac7e7ded7fcdd7e9a2f9ea6cd0dc7c6c7.

--- src/resources/views/components/layout.blade.php
+++ src/resources/views/components/layout.blade.php
@@ -57,6 +57,7 @@
         <meta name="twitter:image" content="{{ url("/250") }}" />
 
         <link rel="canonical" href="{{ url()->current() }}" />
+        <link rel="stylesheet" href="{{ asset("css/app.css") }}" />
 
         <!-- Styles & Scripts -->
         @vite(["resources/css/app.css", "resources/js/app.js"])
